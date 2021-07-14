77c88348db23925a601fde38717bb350a1b9390a
Andrey Piksaev <bernigend@gmail.com>
2021-07-14 21:11:14 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,358 @@
+<?php
+
+require_once 'src/Approximation.php';
+
+if (isset($_GET['calculate'])) {
+    header('Content-Type: application/json');
+
+    if (!isset($_FILES['input_csv'])) {
+        die('no input file');
+    }
+
+    $x = [];
+    $y = [];
+
+    $handle = @fopen($_FILES['input_csv']['tmp_name'], "r");
+    if ($handle) {
+        while (($buffer = fgets($handle, 4096)) !== false) {
+            $buffer = str_replace([',',';'], ['.',','], $buffer);
+            $data = json_decode('['.$buffer.']');
+            $x[] = $data[0];
+            $y[] = $data[1];
+        }
+        if (!feof($handle)) {
+            echo "Ошибка: fgets() неожиданно потерпел неудачу\n";
+        }
+        fclose($handle);
+    }
+
+    $plotData = [
+        [
+            'label' => 'source',
+            'clickable' => true,
+            'hoverable' => true,
+            'data' => [],
+        ],
+        [
+            'label' => 'result',
+            'clickable' => true,
+            'hoverable' => true,
+            'data' => [],
+        ],
+    ];
+
+    $ort = (new Approximation($x, $y, (int)$_REQUEST['measure'] ?? 2))->init();
+    $result = $ort->getRegress();
+
+    foreach ($x as $xKey => $xPoint) {
+        $plotData[0]['data'][] = [$xPoint, $y[$xKey]];
+        $plotData[1]['data'][] = [$xPoint, $result[$xKey]];
+    }
+
+    $coefficients = $ort->getCoefficients();
+    array_walk($coefficients, static function (&$arCoefficients) {
+        $arCoefficients = array_map(static function ($arItem) {
+            return sprintf('%.6f', $arItem);
+        }, $arCoefficients);
+    });
+
+    $data = [
+        'plot' => $plotData,
+        'coefficients' => $coefficients,
+        'discrepancy' => $ort->getDiscrep($result),
+    ];
+
+    echo json_encode($data);
+    return;
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script type="text/javascript" src="./js/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/jquery.flot.js"></script>
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container">
+    <h3 class="mt-3">Практика. МНК</h3>
+
+    <form action="?calculate" id="form" enctype="multipart/form-data" class="mt-5">
+        <div class="mb-3 row">
+            <label for="inputMeasure" class="col-sm-2 col-form-label">Полином</label>
+            <div class="col-sm-10">
+                <input type="number" name="measure" id="inputMeasure" class="form-control" value="10" required>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="inputFile" class="col-sm-2 col-form-label">Входной файл</label>
+            <div class="col-sm-10">
+                <input type="file" id="inputFile" name="input_csv" class="form-control" required>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <div class="col-sm-3 text-center">
+                <b>Скачать примеры файлов:</b>
+            </div>
+            <div class="col-sm-3 text-center">
+                <a href="input1.csv" download="true">input1.csv</a>
+            </div>
+            <div class="col-sm-3 text-center">
+                <a href="input2.csv" download="true">input2.csv</a>
+            </div>
+            <div class="col-sm-3 text-center">
+                <a href="input3.csv" download="true">input3.csv</a>
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <button type="submit" class="btn btn-primary mb-3">Продолжить</button>
+        </div>
+    </form>
+
+    <!-- Modal -->
+    <div class="modal fade" id="resultModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Результаты</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="placeholder" style="width:100%;height:500px"></div>
+                    <div class="mt-5"></div>
+
+                    <div class="mb-3 row">
+                        <label for="inputMeasure" class="col-sm-6 col-form-label">Величина макс. относительной ошибки</label>
+                        <div class="col-sm-6">
+                            <input type="number" readonly id="outputDiscrepancy" class="form-control-plaintext" value="0">
+                        </div>
+                    </div>
+
+                    <div class="m-3 row">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">коэффициенты при степенях регрессионного полинома</th>
+                            </tr>
+                            </thead>
+                            <tbody id="coefficients-table"></tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(function () {
+        const resultModal = new bootstrap.Modal(document.getElementById('resultModal'));
+
+        $('#form').on('submit', function (event) {
+            event.preventDefault();
+
+            const $form = $(this);
+            const formData = new FormData(this);
+
+            $("#placeholder").html('');
+
+            $.ajax({
+                type: 'POST',
+                url: $form.attr('action'),
+                data: formData,
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+            }).done(function (data) {
+                console.log(data);
+
+                resultModal.show();
+                setTimeout(function () {
+                    data.plot = data.plot || {};
+                    $.plot("#placeholder", data.plot, {
+                        series: {
+                            lines: {
+                                show: true
+                            },
+                            points: {
+                                show: true
+                            }
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true
+                        },
+                    });
+
+                    data.discrepancy = data.discrepancy || '';
+                    $("#outputDiscrepancy").val(data.discrepancy || '');
+
+                    data.coefficients = data.coefficients || {};
+                    data.coefficients.a = data.coefficients.a || [];
+                    data.coefficients.b = data.coefficients.b || [];
+                    data.coefficients.c = data.coefficients.c || [];
+
+                    const $coefficientsTable = $("#coefficients-table");
+                    $coefficientsTable.html('');
+
+                    data.coefficients.a.forEach(function (currentValue, index) {
+                        let a = currentValue;
+                        let b = typeof data.coefficients.b[index] !== "undefined" ? data.coefficients.b[index] : 0;
+                        let c = typeof data.coefficients.c[index] !== "undefined" ? data.coefficients.c[index] : 0;
+
+                        $("<tr><th scope='row'>"+index+"</th><td>"+a+"</td></tr>").appendTo($coefficientsTable);
+
+                        console.log(a, b, c);
+                    });
+
+                }, 1000);
+            }).fail(console.error);
+        });
+
+        $("<div id='tooltip'></div>").css({
+            position: "absolute",
+            display: "none",
+            border: "1px solid #fdd",
+            padding: "2px",
+            "background-color": "#fee",
+            opacity: 0.80,
+            "z-index": 9999,
+        }).appendTo("body");
+
+        $("#placeholder").bind("plothover", function (event, pos, item) {
+            if (item) {
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+
+                $("#tooltip").html(item.series.label + " of " + x + " = " + y)
+                    .css({top: item.pageY+5, left: item.pageX+5})
+                    .fadeIn(200);
+            } else {
+                $("#tooltip").hide();
+            }
+        });
+    });
+</script>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
+
+<?php return ?>
+
+<html>
+<head>
+    <title>Тест графиков</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="./js/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/jquery.flot.js"></script>
+</head>
+<body>
+<form action="?calculate" id="form" enctype="multipart/form-data">
+    <label>
+        <span>CSV file:</span> <br>
+        <input type="file" name="input_csv">
+    </label>
+    <br>
+    <label>
+        <span>Полином</span>
+        <input type="text" name="measure" value="5">
+    </label>
+    <br>
+    <input type="submit" value="Продолжить">
+</form>
+<div>
+    <h3>Примеры файлов с данными</h3>
+    <a href="./input1.csv" download="true">input1.csv</a> <br>
+    <a href="./input2.csv" download="true">input2.csv</a> <br>
+    <a href="./input3.csv" download="true">input3.csv</a> <br>
+</div>
+<div id="placeholder" style="width:100%;height:500px"></div>
+<script>
+    $(function () {
+        $('#form').on('submit', function (event) {
+            event.preventDefault();
+
+            const $form = $(this);
+            const formData = new FormData(this);
+
+            $.ajax({
+                type: 'POST',
+                url: $form.attr('action'),
+                data: formData,
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+            }).done(function (data) {
+                console.log(data);
+                $.plot("#placeholder", data.plot, {
+                    series: {
+                        lines: {
+                            show: true
+                        },
+                        points: {
+                            show: true
+                        }
+                    },
+                    grid: {
+                        hoverable: true,
+                        clickable: true
+                    },
+                });
+            }).fail(console.error);
+        });
+
+        $("<div id='tooltip'></div>").css({
+            position: "absolute",
+            display: "none",
+            border: "1px solid #fdd",
+            padding: "2px",
+            "background-color": "#fee",
+            opacity: 0.80
+        }).appendTo("body");
+
+        $("#placeholder").bind("plothover", function (event, pos, item) {
+            if (item) {
+                var x = item.datapoint[0].toFixed(2),
+                    y = item.datapoint[1].toFixed(2);
+
+                $("#tooltip").html(item.series.label + " of " + x + " = " + y)
+                    .css({top: item.pageY+5, left: item.pageX+5})
+                    .fadeIn(200);
+            } else {
+                $("#tooltip").hide();
+            }
+        });
+    });
+</script>
+</body>
+</html>
+
---
+++ src/Approximation.php
@@ -0,0 +1,275 @@
+<?php
+
+/**
+ * Задача о полиномиальной регрессии (аппроксимации табличной функции) требует большой аккуратности,
+ * поскольку при обычных методах решения расчёт главного определителя СЛАУ (определителя Вандермонда)
+ * приводит к вычитанию близких чисел и в итоге снижает относительную погрешность вычислений,
+ * что проявляется уже для полиномов 8-10 порядка.
+ *
+ * От этих недостатков свободны методы разложения в ряды по ортогональным функциям
+ * (например, разложение в ряды Фурье по косинусам), и хотелось бы так же просто обращаться с полиномами.
+ * Однако классические ортогональные полиномы (Лежандра, Чебышёва и пр.) в дискретном варианте ортогональны
+ * только на специально подобранных неравномерных сетках.
+ *
+ * Удачный выход из такой ситуации даёт малоизвестный метод ортогональной полиномиальной регрессии
+ * (Orthogonal Polynomial Curve Fitting, Jeff Reid),
+ * в котором сначала по абсциссам заданных точек конструируются ортогональные полиномы,
+ * а затем с помощью МНК рассчитываются коэффициенты при этих полиномах.
+ */
+
+
+/**
+ * Class Approximation.
+ */
+class Approximation
+{
+    /** @var float[] */
+    protected $xPoints = [];
+    /** @var float[] */
+    protected $yPoints = [];
+    /** @var int */
+    protected $level = 1;
+    /** @var float[][] */
+    protected $values = [];
+    /** @var float[] */
+    protected $sums = [];
+    /** @var float[] */
+    protected $coefficients = [];
+
+    /**
+     * Approximation constructor.
+     * @param array $xPoints
+     * @param array $yPoints
+     * @param int $level
+     */
+    public function __construct(
+        array $xPoints,
+        array $yPoints,
+        int $level
+    )
+    {
+        $this->xPoints = $xPoints;
+        $this->yPoints = $yPoints;
+        $this->level = $level;
+    }
+
+    public function init(): self
+    {
+        $this->calcValuesSums();
+        $this->calcAllCoefficients();
+
+        return $this;
+    }
+
+    protected function calcValuesSums(): void
+    {
+        for ($j = -1; $j < $this->level; $j++) {
+            if (!isset($v))
+            {
+                $v = [array_fill(0, count($this->xPoints), 1.0)];
+                $s = [(float)count($this->xPoints)];
+                $q = [array_sum($this->xPoints)];
+            }
+            else
+            {
+                $v[] = $this->subtract($this->xPoints, end($q)/end($s));
+                if ($j > 0) {
+                    $v[$j+1] = $this->subtract(
+                        $this->multi(end($v), prev($v)),
+                        $this->multi(prev($v), end($s)/prev($s))
+                    );
+                }
+
+                $sq = $this->multi(end($v), 'square');
+                $s[] = array_sum($sq);
+                $q[] = array_sum($this->multi($this->xPoints, $sq));
+            }
+        }
+
+        $this->values = $v ?? [];
+        $this->sums = [
+            '&sum;P&sup2;'  => $s ?? [],
+            '&sum;xP&sup2;' => $q ?? [],
+        ];
+    }
+
+    private function calcAllCoefficients()
+    {
+        $s = reset($this->sums);
+        $b = [];
+
+        foreach ($this->values as $key => $v) {
+            $b[] = array_sum($this->multi($this->yPoints, $v))/$s[$key];
+        }
+
+        $c = $this->calcOrthoCoeffs();
+        $m = count($b) - 1;
+        $a = array_fill(0, $m+1, 0.0);
+
+        foreach ($a as $j => &$aj) {
+            for ($k = $j; $k <= $m; $k++) {
+                $aj += $b[$k] * $c[$k][$j];
+            }
+        }
+
+        $this->coefficients = ['c' => $c, 'b'=> $b, 'a'=> $a];
+    }
+
+    /**
+     * @param array $array
+     * @param $subtract
+     * @return array
+     */
+    protected function subtract(array $array, $subtract): array
+    {
+        if (is_array($subtract)) {
+            return array_map(function($val1, $val2) {
+                return $val1 - $val2;
+            }, $array, $subtract);
+        }
+
+        return array_map(function($val) use($subtract) {
+            return $val - $subtract;
+        }, $array);
+    }
+
+    /**
+     * Перемножает элементы первого массива.
+     * @param array $array
+     * @param $multi
+     * @return float[]
+     */
+    protected function multi(array $array, $multi): array
+    {
+        if (is_array($multi))
+        {
+            return array_map(function($a, $b) {
+                return $a * $b;
+            }, $array, $multi);
+        }
+        elseif ($multi === 'square')
+        {
+            return array_map(function($a) {
+                return $a * $a;
+            }, $array);
+        }
+
+        return array_map(function($val) use($multi) {
+            return $val * $multi;
+        }, $array);
+    }
+
+    /**
+     * @return float[][]
+     */
+    protected function calcOrthoCoeffs(): array
+    {
+        $prev = null;
+
+        $ab = array_map(function($ss, $qq) use (&$prev) {
+            if (is_null($prev)) {
+                $bb = 0;
+            } else {
+                $bb = $ss / $prev;
+            }
+            $prev = $ss;
+            return [$qq / $ss, $bb];
+        }, (array)reset($this->sums), next($this->sums));
+
+        foreach ($ab as $k => list($aa, $bb)) {
+            if (!isset($c)) {
+                $c = [[1.0]];
+            }
+
+            $c[] = array_fill(0, $k+2, 1.0);
+
+            for ($j = 0; $j <= $k; $j++) {
+                $c[$k+1][$j] = (($j==0 ? 0.0 : $c[$k][$j-1]) - $c[$k][$j] * $aa - (($j==$k))
+                    ? 0.0
+                    : $c[$k-1][$j] * $bb);
+            }
+        }
+
+        return $c ?? [];
+    }
+
+    /**
+     * @return float[]
+     */
+    private function calcRegress(): array
+    {
+        foreach ($this->values as $key => $arr) {
+            if (!isset($p)) {
+                $p = $this->multi($arr, $this->coefficients['b'][$key]);
+            } else {
+                $p = $this->subtract($p, $this->multi($arr, - $this->coefficients['b'][$key]));
+            }
+        }
+
+        return $p ?? [];
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getRegress(): array
+    {
+        return $this->calcRegress();
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getXPoints(): array
+    {
+        return $this->xPoints;
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getYPoints(): array
+    {
+        return $this->yPoints;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    /**
+     * @return float[][]
+     */
+    public function getValues(): array
+    {
+        return $this->values;
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getSums(): array
+    {
+        return $this->sums;
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getCoefficients(): array
+    {
+        return $this->coefficients;
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getDiscrep(array $values)
+    {
+        return max(array_map('abs', $this->subtract($this->yPoints, $values)));
+    }
+}
